ab21788149e2664d49b976a14a91e0965a5a0605
Travis <travisformayor@users.noreply.github.com>
2017-06-20 15:11:24 -0700

    Update vending.js

--- js/vending.js
+++ js/vending.js
@@ -13,14 +13,14 @@
     let card10addr   = "0x72b34d637C0d14acE58359Ef1bF472E4b4c57125";
     let card10vend1addr = "0x46C4723111e2bAFeF7d5d0664B3F7BC68D875DeA";
 
-    let card17addr   = "0xE0B5E6F32d657e0e18d4B3E801EBC76a5959e123";
-    let card17vend1addr = "0xA314D1C078239D4571d564a9e0ba6E2B4C32FBF2";
+    let card17addr   = "0xF59536290906F204C3c7918D40C1Cc5f99643d0B";
+    let card17vend1addr = "0xFc370fDD6a5441A7B367099645E4393c904F7c94";
 
-    let card18addr   = "0x8ccf904E75bC592DF3dB236cD171D0CAF0B2bbCB";
-    let card18vend1addr = "0x9Bd4F745e5052058718803D07380aa8c87AEBD2a";
+    let card18addr   = "0xA507D9d28bbca54cBCfFad4BB770C2EA0519F4F0";
+    let card18vend1addr = "0x52D38D9D852db7D072b4F32768037771B97EC367";
 
-    let card19addr   = "0x6b3485C6Eb9027F4f02e495f42708b5896E2579B";
-    let card19vend1addr = "0x8Bc133A6026522E79f78c897893911BBe755Bb4d";
+    let card19addr   = "0xf26BC97Aa8AFE176e275Cf3b08c363f09De371fA";
+    let card19vend1addr = "0x0E7C091138A9abF27927326bE457B3F57Db5184D";
 
     //outputs
     //vending machine quantity values output
