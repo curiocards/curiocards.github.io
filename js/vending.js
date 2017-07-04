ecaea2b127f1de1a2fda8941251cfa065b9aa7da
Travis <travisformayor@users.noreply.github.com>
2017-07-04 08:32:29 -0700

    Add files via upload

--- js/vending.js
+++ js/vending.js
@@ -13,35 +13,19 @@
     let card10addr   = "0x72b34d637C0d14acE58359Ef1bF472E4b4c57125";
     let card10vend1addr = "0x46C4723111e2bAFeF7d5d0664B3F7BC68D875DeA";
 
-    let card17addr   = "0xF59536290906F204C3c7918D40C1Cc5f99643d0B";
-    let card17vend1addr = "0xFc370fDD6a5441A7B367099645E4393c904F7c94";
-
-    let card18addr   = "0xA507D9d28bbca54cBCfFad4BB770C2EA0519F4F0";
-    let card18vend1addr = "0x52D38D9D852db7D072b4F32768037771B97EC367";
-
-    let card19addr   = "0xf26BC97Aa8AFE176e275Cf3b08c363f09De371fA";
-    let card19vend1addr = "0x0E7C091138A9abF27927326bE457B3F57Db5184D";
+    let card20addr   = "0xD0ec99E99cE22f2487283A087614AEe37F6B1283";
+    let card20vend1addr = "0x6E561B66f393166d611f40A1e82E373f00270393";
 
     //outputs
     //vending machine quantity values output
     $('#output10vend1supply').html(MyContract.at(card10vend1addr).available().toString());
-    $('#output17vend1supply').html(MyContract.at(card17vend1addr).available().toString());
-    $('#output17vend1supply').html(MyContract.at(card17vend1addr).available().toString());
-    $('#output18vend1supply').html(MyContract.at(card18vend1addr).available().toString());
-    $('#output18vend1supply').html(MyContract.at(card18vend1addr).available().toString());
-    $('#output19vend1supply').html(MyContract.at(card19vend1addr).available().toString());
-    $('#output19vend1supply').html(MyContract.at(card19vend1addr).available().toString());
+    $('#output20vend1supply').html(MyContract.at(card20vend1addr).available().toString());
 
     //card address output for site
     $('#outputcard10addr').html(card10addr);
     $('#outputcard10vend1addr').html(card10vend1addr);
 
-    $('#outputcard17addr').html(card17addr);
-    $('#outputcard17vend1addr').html(card17vend1addr);
+    $('#outputcard20addr').html(card20addr);
+    $('#outputcard20vend1addr').html(card20vend1addr);
 
-    $('#outputcard18addr').html(card18addr);
-    $('#outputcard18vend1addr').html(card18vend1addr);
-
-    $('#outputcard19addr').html(card19addr);
-    $('#outputcard19vend1addr').html(card19vend1addr);
   });
